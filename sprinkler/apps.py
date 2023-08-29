ece359766fc0634550476be8972a6ff480c3425d
Hunter Myers <39090768+SlightlyBuggy@users.noreply.github.com>
2023-08-29 11:37:11 -0500

    Update apps.py

--- sprinkler/apps.py
+++ sprinkler/apps.py
@@ -5,7 +5,7 @@ class SprinklerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sprinkler'
 
-    # def ready(self):
-    #     from sprinkler import mqtt
+    def ready(self):
+        from sprinkler import mqtt
 
-    #     mqtt.client.loop_start()
+        mqtt.client.loop_start()
