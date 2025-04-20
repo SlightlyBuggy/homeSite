fdfb94df57dd846fb99ec00e1f7b47572ba01822
Hunter Myers <myers.hunter@gmail.com>
2025-04-20 14:43:11 -0500

    healed case where a precip event does not have a start time

--- sprinkler/service/weather_service.py
+++ sprinkler/service/weather_service.py
@@ -2,8 +2,7 @@ import requests
 from sprinkler.classes.PrecipObservations import PrecipObservations
 from sprinkler.models import RainLog
 from sprinkler.service import schedule_service
-import json
-import os
+from datetime import timedelta
 
 
 def convert_mm_to_in(length_in_mm):
@@ -36,12 +35,14 @@ def get_and_record_precip_observations(test_observations=None) -> PrecipObservat
 
 def create_rain_logs_from_precip_observations(precip_observations) -> bool:
     new_log_created = False
-    # TODO: need to heal the case where the very first entry in the DB has precip happening.  it won't have a start time
+
     # due to no rising edge.  need to give it a start time
     for precip_event in precip_observations.precip_events:
 
         # check for a precip event with this start time.  if we have it, update its data
-        matching_rain_logs: list[RainLog] = RainLog.objects.filter(start_time=precip_event.start)
+        matching_rain_logs: list[RainLog] = []
+        if precip_event.start:
+            matching_rain_logs: list[RainLog] = RainLog.objects.filter(start_time=precip_event.start)
 
         if matching_rain_logs:
             matching_rain_log = matching_rain_logs[0]
@@ -49,6 +50,9 @@ def create_rain_logs_from_precip_observations(precip_observations) -> bool:
             matching_rain_log.total_amount_inches = convert_mm_to_in(precip_event.total_mm)
             matching_rain_log.save()
         else:
+            # heal case where there is no start time
+            if not precip_event.start and precip_event.end:
+                precip_event.start = precip_event.end + timedelta(hours=-1)
             new_rain_log = RainLog(start_time=precip_event.start, end_time=precip_event.end,
                                    total_amount_inches=convert_mm_to_in(precip_event.total_mm))
             new_rain_log.save()
